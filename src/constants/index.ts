e9d2860149127fd2f213746a49d6123343bb2cc0
crisog <ortega.cpp@gmail.com>
2021-06-02 15:12:52 -0400

    fix: suggested bases & stablecoin addresses

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 import { AbstractConnector } from '@web3-react/abstract-connector'
-import { ChainId, JSBI, Percent, CurrencyAmount, WETH, WSPOA, DXD, WXDAI, Token, Currency, WMATIC } from 'dxswap-sdk'
+import { ChainId, JSBI, Percent, CurrencyAmount, WETH, WSPOA, WXDAI, Token, Currency, WMATIC } from 'dxswap-sdk'
 import { tokens } from './tokens'
 import { authereum, injected, walletConnect } from '../connectors'
 
@@ -10,7 +10,17 @@ type ChainTokenList = {
   readonly [chainId in ChainId]: Token[]
 }
 
-export const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'Dai Stablecoin')
+export const DAI: { [key: number]: Token } = {
+  [ChainId.MAINNET]: new Token(
+    ChainId.MAINNET,
+    '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    18,
+    'DAI',
+    'Dai Stablecoin'
+  ),
+  [ChainId.XDAI]: new Token(ChainId.XDAI, '0x44fa8e6f47987339850636f88629646662444217', 18, 'DAI', 'Dai Stablecoin'),
+  [ChainId.MATIC]: new Token(ChainId.MATIC, '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063', 18, 'DAI', 'Dai Stablecoin')
+}
 
 export const USDC: { [key: number]: Token } = {
   [ChainId.MAINNET]: new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6, 'USDC', 'USD//C'),
@@ -20,7 +30,8 @@ export const USDC: { [key: number]: Token } = {
     6,
     'USDC',
     'USD//C from Ethereum'
-  )
+  ),
+  [ChainId.MATIC]: new Token(ChainId.MATIC, '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', 6, 'USDC', 'PoS USDC')
 }
 
 export const USDT: { [key: number]: Token } = {
@@ -31,7 +42,8 @@ export const USDT: { [key: number]: Token } = {
     6,
     'USDT',
     'Tether USD from Ethereum'
-  )
+  ),
+  [ChainId.MATIC]: new Token(ChainId.MATIC, '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', 6, 'USDT', 'PoS Tether USD')
 }
 
 export const WBTC: { [key: number]: Token } = {
@@ -51,7 +63,10 @@ export const WBTC: { [key: number]: Token } = {
   )
 }
 
-export const HONEY = new Token(ChainId.XDAI, '0x71850b7e9ee3f13ab46d67167341e4bdc905eef9', 18, 'HNY', 'Honey')
+export const HONEY: { [key: number]: Token } = {
+  [ChainId.XDAI]: new Token(ChainId.XDAI, '0x71850b7e9ee3f13ab46d67167341e4bdc905eef9', 18, 'HNY', 'Honey'),
+  [ChainId.MATIC]: new Token(ChainId.MATIC, '0xb371248dd0f9e4061ccf8850e9223ca48aa7ca4b', 18, 'HNY', 'Honey')
+}
 
 export const STAKE = new Token(
   ChainId.XDAI,
@@ -75,8 +90,7 @@ export const AGAVE = new Token(ChainId.XDAI, '0x3a97704a1b25F08aa230ae53B352e2e7
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   [ChainId.MAINNET]: [
     WETH[ChainId.MAINNET],
-    DXD[ChainId.MAINNET],
-    DAI,
+    DAI[ChainId.MAINNET],
     USDC[ChainId.MAINNET],
     WBTC[ChainId.MAINNET],
     USDT[ChainId.MAINNET]
@@ -87,43 +101,56 @@ export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   [ChainId.XDAI]: [
     WXDAI[ChainId.XDAI],
     WETH[ChainId.XDAI],
-    DXD[ChainId.XDAI],
     USDC[ChainId.XDAI],
     USDT[ChainId.XDAI],
     WBTC[ChainId.XDAI],
-    HONEY,
+    HONEY[ChainId.XDAI],
     STAKE,
     AGAVE,
     BAO
   ],
-  [ChainId.MATIC]: [WETH[ChainId.MATIC], WMATIC[ChainId.MATIC]]
+  [ChainId.MATIC]: [
+    WETH[ChainId.MATIC],
+    WMATIC[ChainId.MATIC],
+    HONEY[ChainId.MATIC],
+    DAI[ChainId.MATIC],
+    USDC[ChainId.MATIC],
+    USDT[ChainId.MATIC]
+  ]
 }
 
 // used for display in the default list when adding liquidity (native currency is already shown
 // by default, so no need to add the wrapper to the list)
 export const SUGGESTED_BASES: ChainTokenList = {
-  [ChainId.MAINNET]: [DXD[ChainId.MAINNET], DAI, USDC[ChainId.MAINNET], USDT[ChainId.MAINNET], WBTC[ChainId.MAINNET]],
+  [ChainId.MAINNET]: [DAI[ChainId.MAINNET], USDC[ChainId.MAINNET], USDT[ChainId.MAINNET], WBTC[ChainId.MAINNET]],
   [ChainId.RINKEBY]: [],
   [ChainId.ARBITRUM_TESTNET_V3]: [],
   [ChainId.SOKOL]: [],
-  [ChainId.XDAI]: [DXD[ChainId.XDAI], WETH[ChainId.XDAI], USDC[ChainId.XDAI]],
-  [ChainId.MATIC]: [WETH[ChainId.MATIC]]
+  [ChainId.XDAI]: [WETH[ChainId.XDAI], USDC[ChainId.XDAI], HONEY[ChainId.XDAI]],
+  [ChainId.MATIC]: [WETH[ChainId.MATIC], HONEY[ChainId.MATIC]]
 }
 
 // used to construct the list of all pairs we consider by default in the frontend
 export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
-  [ChainId.MAINNET]: [WETH[ChainId.MAINNET], DXD[ChainId.MAINNET], DAI, USDC[ChainId.MAINNET], USDT[ChainId.MAINNET]],
+  [ChainId.MAINNET]: [WETH[ChainId.MAINNET], DAI[ChainId.MAINNET], USDC[ChainId.MAINNET], USDT[ChainId.MAINNET]],
   [ChainId.RINKEBY]: [WETH[ChainId.RINKEBY]],
   [ChainId.ARBITRUM_TESTNET_V3]: [WETH[ChainId.ARBITRUM_TESTNET_V3]],
   [ChainId.SOKOL]: [Token.WSPOA[ChainId.SOKOL]],
-  [ChainId.XDAI]: [WXDAI[ChainId.XDAI], DXD[ChainId.XDAI], WETH[ChainId.XDAI], USDC[ChainId.XDAI], STAKE],
-  [ChainId.MATIC]: [WMATIC[ChainId.MATIC], WETH[ChainId.MATIC]]
+  [ChainId.XDAI]: [WXDAI[ChainId.XDAI], WETH[ChainId.XDAI], USDC[ChainId.XDAI], STAKE],
+  [ChainId.MATIC]: [
+    WMATIC[ChainId.MATIC],
+    WETH[ChainId.MATIC],
+    HONEY[ChainId.MATIC],
+    DAI[ChainId.MATIC],
+    USDC[ChainId.MATIC],
+    USDT[ChainId.MATIC]
+  ]
 }
 
 export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
   [ChainId.MAINNET]: [
     [USDC[ChainId.MAINNET], USDT[ChainId.MAINNET]],
-    [DAI, USDT[ChainId.MAINNET]]
+    [DAI[ChainId.MAINNET], USDT[ChainId.MAINNET]]
   ]
 }
 
