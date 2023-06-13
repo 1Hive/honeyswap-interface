9d16e279295d60a9e103469e0a13cd901ccbed56
Felipe Novaes F Rocha <windholyghost@gmail.com>
2023-06-13 13:27:07 -0300

    :adhesive_bandage: marquee put in place.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState, useEffect } from 'react'
 import { Route, Switch, HashRouter } from 'react-router-dom'
 import styled from 'styled-components'
 import Header from '../components/Header'
@@ -13,6 +13,9 @@ import RemoveLiquidity from './RemoveLiquidity'
 import { RedirectOldRemoveLiquidityPathStructure } from './RemoveLiquidity/redirects'
 import Swap from './Swap'
 import { RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
+import { useActiveWeb3React } from '../hooks'
+import { ethers } from 'ethers'
+import Marquee, { MARQUEE_CONTRACT_ADDRESS, marqueeAbi } from '../components/Marquee'
 
 const AppWrapper = styled.div`
   display: flex;
@@ -43,6 +46,25 @@ const BodyWrapper = styled.div`
 
   ${({ theme }) => theme.mediaWidth.upToSmall`
     padding: 16px;
+    
+  `};
+
+  z-index: 1;
+`
+
+const MarqueeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 16px;
+  align-items: center;
+  flex: 1;
+  overflow-y: auto;
+  overflow-x: hidden;
+  z-index: 10;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    padding: 16px;
     padding-top: 2rem;
   `};
 
@@ -58,10 +80,31 @@ const Footer = styled.div`
   text-align: center;
   width: 420px;
   margin: auto;
+  padding-bottom: 16px
   color: #fffa;
 `
 
 export default function App() {
+  const { library } = useActiveWeb3React()
+  const [currentMarquee, setCurrentMarquee] = useState('')
+  const handleMarqueeUpdate = (newMarquee: string) => {
+    setCurrentMarquee(newMarquee)
+  }
+
+  useEffect(() => {
+    if (library) {
+      const contract = new ethers.Contract(MARQUEE_CONTRACT_ADDRESS, marqueeAbi, library)
+      contract
+        .marquee()
+        .then((marquee: string) => {
+          setCurrentMarquee(marquee)
+        })
+        .catch((error: Error) => {
+          console.error('Error retrieving marquee:', error)
+        })
+    }
+  }, [library])
+
   return (
     <Suspense fallback={null}>
       <HashRouter>
@@ -90,11 +133,23 @@ export default function App() {
                 <Route component={RedirectPathToSwapOnly} />
               </Switch>
             </Web3ReactManager>
+            <MarqueeWrapper>
+              <Marquee marquee={currentMarquee} onUpdate={handleMarqueeUpdate} />
+            </MarqueeWrapper>
             <Marginer />
           </BodyWrapper>
 
           <Footer>
-            “That which is not good for the <span role="img" aria-label="bee">🐝</span>-hive cannot be good for the <span role="img" aria-label="bee">🐝</span>.”  —Marcus Aurelius</Footer>
+            “That which is not good for the{' '}
+            <span role="img" aria-label="bee">
+              🐝
+            </span>
+            -hive cannot be good for the{' '}
+            <span role="img" aria-label="bee">
+              🐝
+            </span>
+            .” —Marcus Aurelius
+          </Footer>
         </AppWrapper>
       </HashRouter>
     </Suspense>
